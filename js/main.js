da7fc2aca10d05dfe82917380f8f550557b60fbd
Alexandr Porubinovskiy <sp-01@i.ua>
2022-05-10 00:56:18 +0300

    was remooved modular architecture from code

--- js/main.js
+++ js/main.js
@@ -176,8 +176,31 @@ function agrementChecker(event) {
     }
 }
 
-import popupClass from "/js/modules/module-popup.js";
+class PopUp {
+    constructor (selector_container, selector_content, selector_mainContent) {
+        this.popupDiscription_container = document.querySelector(selector_container);
+        this.popupDiscription_content = document.querySelector(selector_content);
+        this.mainContent = document.querySelector(selector_mainContent);
+        this.body = document.querySelector("body");
+    }
 
-let popupDiscription = new popupClass (".popupDiscription-container", ".popupDiscription-content", ".mainContent");
+    up () {        
+        let bodyScrollBarWidth = window.innerWidth - this.body.clientWidth;
+        this.body.style.paddingRight = parseInt(getComputedStyle(this.mainContent).paddingRight) + bodyScrollBarWidth + "px";
+        this.popupDiscription_container.classList.add("active");
+        this.popupDiscription_container.addEventListener("click", this.down.bind(this), { once: true });
+        this.mainContent.classList.add("freezed");
+        this.body.classList.add("freezed");
+    }
+    
+    down () {        
+        this.popupDiscription_container.classList.remove("active");
+        this.mainContent.classList.remove("freezed");
+        this.body.classList.remove("freezed");
+        this.body.style.paddingRight = "";
+    }
+}
+
+let popupDiscription = new PopUp (".popupDiscription-container", ".popupDiscription-content", ".mainContent");
 
 window.onload = popupDiscription.up.bind(popupDiscription);
